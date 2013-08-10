23e78a786c293b66a417f87e62ab9eca1bef23d0
Dominik Dumaine <d_dumaine@yahoo.com>
2013-08-10 06:45:31 +0000

    Edited product description in index.html

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
 var express = require('express');
-var app = express();
+var app = express.createServer(express.logger());
 var fs = require('fs');
 
-app.use(express.logger());
-
-filename = "index.html";
+var filename = "index.html";
 var buf = fs.readFileSync(filename,'utf-8');
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
